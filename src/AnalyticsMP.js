8a9777c4a5f874b6ff6246f2c86f47b240eb9d4f
Romain Vialard <romain.vialard@gmail.com>
2020-10-06 09:34:01 +0200

    Update AnalyticsMP.js

--- src/AnalyticsMP.js
+++ src/AnalyticsMP.js
@@ -24,18 +24,18 @@
  *                                                          or simply to switch to another property store (Document / Script)
  */
 function sendAnalyticsEvent(parameters, optPropertyStore) {
-	var options = {
-		method: 'post',
-		payload: AnalyticsMP_._addRequiredParameters(parameters, optPropertyStore, true)
-	};
-	
-	// if the ErrorHandler library is loaded, use it (https://github.com/RomainVialard/ErrorHandler)
-	if (this['ErrorHandler']) {
-		ErrorHandler.urlFetchWithExpBackOff('https://www.google-analytics.com/collect', options);
-	}
-	else {
-		UrlFetchApp.fetch('https://www.google-analytics.com/collect', options);
-	}
+  var options = {
+    method: 'post',
+    payload: AnalyticsMP_._addRequiredParameters(parameters, optPropertyStore, true)
+  };
+
+  // if the ErrorHandler library is loaded, use it (https://github.com/RomainVialard/ErrorHandler)
+  if (this['ErrorHandler']) {
+    ErrorHandler.urlFetchWithExpBackOff('https://www.google-analytics.com/collect', options);
+  }
+  else {
+    UrlFetchApp.fetch('https://www.google-analytics.com/collect', options);
+  }
 }
 
 /**
@@ -49,15 +49,15 @@ function sendAnalyticsEvent(parameters, optPropertyStore) {
  *                                                          or simply to switch to another property store (Document / Script)
  */
 function generateAnalyticsTrackingUrl(parameters, optPropertyStore) {
-	var params = AnalyticsMP_._addRequiredParameters(parameters, optPropertyStore);
-	
-	var urlParams = [];
-	for (var key in params) {
-		// noinspection JSUnfilteredForInLoop
-		urlParams.push(key + (params[key] === '' ? '' : '='+ encodeURI(params[key])));
-	}
-	
-	return 'https://www.google-analytics.com/collect?'+ urlParams.join('&amp;');
+  var params = AnalyticsMP_._addRequiredParameters(parameters, optPropertyStore);
+
+  var urlParams = [];
+  for (var key in params) {
+    // noinspection JSUnfilteredForInLoop
+    urlParams.push(key + (params[key] === '' ? '' : '=' + encodeURI(params[key])));
+  }
+
+  return 'https://www.google-analytics.com/collect?' + urlParams.join('&amp;');
 }
 
 /**
@@ -76,30 +76,30 @@ function generateAnalyticsTrackingUrl(parameters, optPropertyStore) {
  * @return {string} Unique User ID
  */
 function getAnalyticsClientId(optPropertyStore) {
-	// if client ID is saved in memory, avoid making new call to the property store
-	if (AnalyticsMP_.clientId) return AnalyticsMP_.clientId;
-	
-	// Get default store if not provided
-	optPropertyStore = optPropertyStore || PropertiesService.getUserProperties();
-	
-	// try to get client ID from user properties
-	AnalyticsMP_.clientId = optPropertyStore.getProperty('clientId');
-	
-	if (!AnalyticsMP_.clientId) {
-		AnalyticsMP_.clientId = Utilities.getUuid();
-		optPropertyStore.setProperty('clientId', AnalyticsMP_.clientId);
-	}
-	
-	return AnalyticsMP_.clientId;
+  // if client ID is saved in memory, avoid making new call to the property store
+  if (AnalyticsMP_.clientId) return AnalyticsMP_.clientId;
+
+  // Get default store if not provided
+  optPropertyStore = optPropertyStore || PropertiesService.getUserProperties();
+
+  // try to get client ID from user properties
+  AnalyticsMP_.clientId = optPropertyStore.getProperty('clientId');
+
+  if (!AnalyticsMP_.clientId) {
+    AnalyticsMP_.clientId = Utilities.getUuid();
+    optPropertyStore.setProperty('clientId', AnalyticsMP_.clientId);
+  }
+
+  return AnalyticsMP_.clientId;
 }
 
 
 // noinspection JSUnusedGlobalSymbols, ThisExpressionReferencesGlobalObjectJS
 this['AnalyticsMP'] = {
-	// Add local alias to run the library as normal code
-	sendAnalyticsEvent: sendAnalyticsEvent,
-	getAnalyticsClientId: getAnalyticsClientId,
-	generateAnalyticsTrackingUrl: generateAnalyticsTrackingUrl,
+  // Add local alias to run the library as normal code
+  sendAnalyticsEvent: sendAnalyticsEvent,
+  getAnalyticsClientId: getAnalyticsClientId,
+  generateAnalyticsTrackingUrl: generateAnalyticsTrackingUrl,
 };
 
 
@@ -115,7 +115,7 @@ var AnalyticsMP_ = {};
 AnalyticsMP_._this = this;
 
 AnalyticsMP_.ERROR = {
-	TRACKING_ID_REQUIRED: 'A valid Google Analytics tracking ID is required'
+  TRACKING_ID_REQUIRED: 'A valid Google Analytics tracking ID is required'
 };
 
 
@@ -133,35 +133,35 @@ AnalyticsMP_.ERROR = {
  * @return {AnalyticsMP_.ParameterRef} - The updated parameters object
  */
 AnalyticsMP_._addRequiredParameters = function (parameters, optPropertyStore, sentViaUrlFetch) {
-	var params = JSON.parse(JSON.stringify(parameters));
-	
-	if (!params['tid']) {
-		if (!AnalyticsMP_._this['ANALYTICS_TRACKING_ID']) throw new Error(AnalyticsMP_.ERROR.TRACKING_ID_REQUIRED);
-		
-		params['tid'] = AnalyticsMP_._this['ANALYTICS_TRACKING_ID'];
-	}
-	
-	params['cid'] = getAnalyticsClientId(optPropertyStore);
-	
-	// The type of hit. Must be one of 'pageview', 'screenview', 'event', 'transaction', 'item', 'social', 'exception', 'timing'.
-	// If none is provided, fall back on 'event'
-	params['t'] = params['t'] || 'event';
-	
-	// if no event label is provided the event might not be visible everywhere - better to provide an empty string
-	params['t'] === 'event' && !params['el'] && (params['el'] = '');
-	
-	params['v'] = '1';
-	params['z'] = Math.floor(Math.random() * 10E7);
-	
-	if (sentViaUrlFetch) {
-		// ds indicates the data source of the hit. Can be freely provided.
-		params['ds'] = params['ds'] || 'urlFetch';
-		
-		// aip : Anonymize IP address - make sense as calls are made by Google servers
-		params['aip'] = '1';
-	}
-	
-	return params;
+  var params = JSON.parse(JSON.stringify(parameters));
+
+  if (!params['tid']) {
+    if (!AnalyticsMP_._this['ANALYTICS_TRACKING_ID']) throw new Error(AnalyticsMP_.ERROR.TRACKING_ID_REQUIRED);
+
+    params['tid'] = AnalyticsMP_._this['ANALYTICS_TRACKING_ID'];
+  }
+
+  params['cid'] = getAnalyticsClientId(optPropertyStore);
+
+  // The type of hit. Must be one of 'pageview', 'screenview', 'event', 'transaction', 'item', 'social', 'exception', 'timing'.
+  // If none is provided, fall back on 'event'
+  params['t'] = params['t'] || 'event';
+
+  // if no event label is provided the event might not be visible everywhere - better to provide an empty string
+  params['t'] === 'event' && !params['el'] && (params['el'] = '');
+
+  params['v'] = '1';
+  params['z'] = Math.floor(Math.random() * 10E7);
+
+  if (sentViaUrlFetch) {
+    // ds indicates the data source of the hit. Can be freely provided.
+    params['ds'] = params['ds'] || 'urlFetch';
+
+    // aip : Anonymize IP address - make sense as calls are made by Google servers
+    params['aip'] = '1';
+  }
+
+  return params;
 };
 
 
@@ -170,25 +170,25 @@ AnalyticsMP_._addRequiredParameters = function (parameters, optPropertyStore, se
 
 /**
  * See all property available here: https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
- * 
- * @typedef {{}} AnalyticsMP_.ParameterRef 
- * 
+ *
+ * @typedef {{}} AnalyticsMP_.ParameterRef
+ *
  * General properties
  * @property {string} v - Protocol Version, set to "1"
  * @property {string} tid - Tracking ID / Web Property ID
- * 
+ *
  * @property {boolean} [aip] - Anonymize IP
  * @property {string} [ds] - Data Source
  * @property {number} [qt] - Queue Time
  * @property {string} [z] - Cache Buster
- * 
+ *
  * User
  * @property {string} [cid] - Client ID
  * @property {string} [uid] - User ID
- * 
+ *
  * Hit
  * @property {'pageview' || 'screenview' || 'event' || 'transaction' || 'item' || 'social' || 'exception' || 'timing'} [t] - Hit type
  * @property {boolean} [ni] - Non-Interaction Hit
- * 
- * And a lot more properties (see online Google Analytics documentation for details 
+ *
+ * And a lot more properties (see online Google Analytics documentation for details
  */
